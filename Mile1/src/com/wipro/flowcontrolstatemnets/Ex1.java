fc25a7cc4819bf1391dca95e550a3d821c35f32b
spandu <spandu@DESKTOP-QQNK2U8>
2020-06-13 15:31:57 +0530

    Ex1

---
+++ Mile1/src/com/wipro/flowcontrolstatemnets/Ex1.java
@@ -0,0 +1,16 @@
+package com.wipro.flowcontrolstatemnets;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+	if(a>0)
+		System.out.println("Positive");
+	else if(a<0)
+		System.out.println("Negative");
+	else
+		System.out.println("Zero");
+
+	}
+
+}
