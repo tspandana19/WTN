bd7381276ad8cd4c47470d36aeee1eeb41c00ebe
spandu <spandu@DESKTOP-QQNK2U8>
2020-06-15 16:56:05 +0530

    Ex13

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex13.java
@@ -0,0 +1,25 @@
+package com.wipro.flowcontrolstatements;
+import java.util.Scanner;
+public class Ex13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number,count=0;
+		System.out.println("Enter the number");
+		Scanner s=new Scanner(System.in);
+		number=s.nextInt();
+		s.close();
+		for(int i=2;i<=number/2;i++) {
+			if(number%i==0)
+				count=1;
+		}
+		if(number==1)
+			System.out.println("Neither prime nor Composite");
+		else if(count==0)
+			System.out.println("Prime number");
+		else
+			System.out.println("Not a prime number");
+
+	}
+
+}
