b5762789eacbb87f1eeb36a1137e8733f406b0dd
spandu <spandu@DESKTOP-QQNK2U8>
2020-06-15 16:49:04 +0530

    Ex10

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex10.java
@@ -0,0 +1,15 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			System.out.print(i);
+		    System.out.print(" ");
+		}
+
+
+	}
+
+}
