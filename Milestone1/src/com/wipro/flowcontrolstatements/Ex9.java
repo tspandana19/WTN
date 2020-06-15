d307dc980d6123f1b4e9ca815149b5e2125319b5
spandu <spandu@DESKTOP-QQNK2U8>
2020-06-15 16:46:56 +0530

    Ex9

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex9.java
@@ -0,0 +1,54 @@
+package com.wipro.flowcontrolstatements;
+import java.util.Scanner;
+public class Ex9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number;
+		System.out.println("Please enter the month in numbers :");
+		Scanner s=new Scanner(System.in);
+		number=s.nextInt();
+		s.close();
+		switch(number) {
+		case 1:
+			System.out.println("January");
+			break;
+		case 2:
+			System.out.println("February");
+			break;	
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		case 6:
+			System.out.println("June");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("August");
+			break;	
+		case 9:
+			System.out.println("September");
+			break;
+		case 10:
+			System.out.println("October");
+			break;
+		case 11:
+			System.out.println("November");
+			break;
+		case 12:
+			System.out.println("December");
+			break;
+		default:System.out.println("Invalid month");	
+		}
+
+	}
+
+}
