3034635d7dc971d0d2cfcea47a888f1722ea5f31
spandu <spandu@DESKTOP-QQNK2U8>
2020-06-15 15:10:02 +0530

    ex1c

---
+++ Milestone1/src/com/wipro/languagebasics/ex1c.java
@@ -0,0 +1,15 @@
+package com.wipro.languagebasics;
+
+public class ex1c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a,b;
+		
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		System.out.println("The sum of 10 and 20 is "+(a+b));
+
+	}
+
+}
