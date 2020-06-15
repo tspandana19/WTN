2d56a30d8831f5acb7617a3c8505d132bc0bbb4b
spandu <spandu@DESKTOP-QQNK2U8>
2020-06-15 14:48:58 +0530

    ex1

---
+++ Milestone1/src/com/wipro/languagebasics/ex1.java
@@ -0,0 +1,10 @@
+package com.wipro.languagebasics;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println(args[0]+" technologies "+args[1]);
+	}
+
+}
